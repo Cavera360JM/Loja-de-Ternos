88d5ae3329f17b294df4ae54c8a0f99cb0ecc5b2
João Miguel <97974646+Cavera360JM@users.noreply.github.com>
2024-02-16 16:44:29 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+
 function showResult(option) {
     var resultDiv = document.getElementById('result');
+    var buttons = document.querySelectorAll('button');
+
+    buttons.forEach(function(button) {
+        button.classList.remove('clicked');
+    });
+
     if (option === 'advertencia') {
-        resultDiv.innerHTML = '<p>Você escolheu ir sem camiseta e tomar advertência.</p>';
+        resultDiv.innerHTML = '<p>Você tomou advertência.</p>';
     } else if (option === 'emprestada') {
-        resultDiv.innerHTML = '<p>Você escolheu pegar emprestada com a empresa ou com algum colega de trabalho.</p>';
+        resultDiv.innerHTML = '<p>Parabéns você conseguiu trabalhar normalmente hoje.</p>';
     }
+
+    event.target.classList.add('clicked');
 }
+    
 
